caa26b562a7000cfbba674d8fe01d3ba5d03db2f
penny <925580783@qq.com>
2020-08-04 19:22:38 +0800

    [todoList] [penny,henry] refactor : refactor component Todo changeDone method

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -2,26 +2,17 @@ import React from 'react';
 import './todo.css'
 
 class Todo extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            isDone: this.props.todo.isDone
-        }
-    }
     delete = () =>{
         console.log(this.props.index);
         this.props.deleteTodo(this.props.index);
     }
     changeDone = () =>{
-        this.setState({
-            isDone:!this.state.isDone
-        })
         this.props.updateTodo(this.props.index);
     }
     render(){
         return(
         <div className="outerTodo" onClick={this.changeDone}>
-            <div className="everyTodo"><span className={this.state.isDone?"isdone":"notdone"}>{this.props.todo.text}</span><span className="rightTodo" onClick={this.delete}><img src="/delete.png" alt="x"/></span></div>
+            <div className="everyTodo"><span className={this.props.todo.isDone?"isdone":"notdone"}>{this.props.todo.text}</span><span className="rightTodo" onClick={this.delete}><img src="/delete.png" alt="x"/></span></div>
         </div>
         )
     }
