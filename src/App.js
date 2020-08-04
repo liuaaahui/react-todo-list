a6c77b7968fcdc4a3195338416cc4de2d22bd82f
penny <925580783@qq.com>
2020-08-04 16:20:25 +0800

    first init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './App.css';
+import Index from './components/Counter/Index';
+
+class App extends Component{
+  render(){
+  return (
+    <div className="App">
+      <Index></Index>
+    </div>
+  );
+}
+}
+export default App;
